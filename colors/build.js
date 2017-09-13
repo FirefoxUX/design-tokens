1ea933f9152aa3128a1f5d0c86856dd02326f25c
Greg Tatum <tatum.creative@gmail.com>
2017-09-13 13:58:17 -0500

    Consolidate createColor function

--- colors/build.js
+++ colors/build.js
@@ -2,32 +2,18 @@ const fs = require('fs');
 const colors = require('./photon.json');
 const metadata = require('./package.json');
 
-function createColor(color, element, prefix) {
+function createColor(color, element, formatter) {
   const rv = [];
 
   for (const variant in element) {
     if (element.hasOwnProperty(variant)) {
       const value = element[variant];
-      rv.push(`${prefix}${color}-${variant}: ${value};\n`);
+      rv.push(formatter(variant, value));
     }
   }
   rv.push('\n');
   return rv;
 }
-
-function createJsColor(color, element) {
-  const rv = [];
-
-  for (const variant in element) {
-    if (element.hasOwnProperty(variant)) {
-      const value = element[variant];
-      rv.push(`exports.${color.toUpperCase()}_${variant} = '${value}';\n`);
-    }
-  }
-  rv.push('\n');
-  return rv;
-}
-
 
 const cssOutput = [`/* Firefox Colors CSS Variables v${metadata.version} */
 
@@ -45,9 +31,12 @@ const jsOutput = [`/* Firefox Colors JS Variables v${metadata.version} */
 for (const color in colors) {
   if (colors.hasOwnProperty(color)) {
     const element = colors[color];
-    cssOutput.push(...createColor(color, element, '  --'));
-    sassOutput.push(...createColor(color, element, '$'));
-    jsOutput.push(...createJsColor(color, element));
+    cssOutput.push(...createColor(color, element,
+      (variant, value) => `  --${color}-${variant}: ${value};\n`));
+    sassOutput.push(...createColor(color, element,
+      (variant, value) => `$${color}-${variant}: ${value};\n`));
+    jsOutput.push(...createColor(color, element,
+      (variant, value) => `exports.${color.toUpperCase()}_${variant} = '${value}';\n`));
   }
 }
 

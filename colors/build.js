52fa0ec8a1e971ed5c0022d00890a39b6e3ceaae
Jonathan <vanillajonathan@users.noreply.github.com>
2017-10-21 00:37:14 +0200

    Remove quotes from properties
    Add missing semicolons.

--- colors/build.js
+++ colors/build.js
@@ -16,53 +16,53 @@ function createColor(color, element, formatter) {
 }
 
 const formats = {
-  'css': {
-    'output': [`/* Firefox Colors CSS Variables v${metadata.version} */
+  css: {
+    output: [`/* Firefox Colors CSS Variables v${metadata.version} */
 
 :root {
 `],
-    'formatter': (color, variant, value) => `  --${color}-${variant}: ${value};\n`,
-    'footer': '}\n',
-    'ext': 'css'
+    formatter: (color, variant, value) => `  --${color}-${variant}: ${value};\n`,
+    footer: '}\n',
+    ext: 'css'
   },
-  'less': {
-    'output': [`/* Firefox Colors Less Variables v${metadata.version} */
+  less: {
+    output: [`/* Firefox Colors Less Variables v${metadata.version} */
 
 `],
-    'formatter': (color, variant, value) => `@${color}-${variant}: ${value};\n`,
-    'ext': 'less'
+    formatter: (color, variant, value) => `@${color}-${variant}: ${value};\n`,
+    ext: 'less'
   },
-  'sass': {
-    'output': [`/* Firefox Colors SCSS Variables v${metadata.version} */
+  sass: {
+    output: [`/* Firefox Colors SCSS Variables v${metadata.version} */
 
 `],
-    'formatter': (color, variant, value) => `$${color}-${variant}: ${value};\n`,
-    'ext': 'scss'
+    formatter: (color, variant, value) => `$${color}-${variant}: ${value};\n`,
+    ext: 'scss'
   },
-  'js': {
-    'output': [`/* Firefox Colors JS Variables v${metadata.version} */
+  js: {
+    output: [`/* Firefox Colors JS Variables v${metadata.version} */
 
 `],
-    'formatter': (color, variant, value) => `exports.${color.toUpperCase()}_${variant} = '${value}';\n`,
-    'ext': 'js'
+    formatter: (color, variant, value) => `exports.${color.toUpperCase()}_${variant} = '${value}';\n`,
+    ext: 'js'
   },
-  'gimp': {
-    'output': [`GIMP Palette
+  gimp: {
+    output: [`GIMP Palette
 Name: Firefox/Photon
 # Firefox Colors GPL Color Palette v${metadata.version}
 # ${metadata.homepage}
 
 `],
-    'formatter': (color, variant, value) => {
+    formatter: (color, variant, value) => {
       const r = parseInt(value.substr(1, 2), 16);
       const g = parseInt(value.substr(3, 2), 16);
       const b = parseInt(value.substr(5, 2), 16);
-      return `${r} ${g} ${b} ${color}-${variant}\n`
+      return `${r} ${g} ${b} ${color}-${variant}\n`;
     },
-    'ext': 'gpl'
+    ext: 'gpl'
   },
-  'libreoffice': {
-    'output': [`<?xml version="1.0" encoding="UTF-8"?>
+  libreoffice: {
+    output: [`<?xml version="1.0" encoding="UTF-8"?>
 <ooo:color-table
   xmlns:office="urn:oasis:names:tc:opendocument:xmlns:office:1.0"
   xmlns:draw="urn:oasis:names:tc:opendocument:xmlns:drawing:1.0"
@@ -71,13 +71,13 @@ Name: Firefox/Photon
   xmlns:ooo="http://openoffice.org/2004/office">
 
 `],
-    'formatter': (color, variant, value) => {
-      return `  <draw:color draw:name="${color}-${variant}" draw:color="${value}" />\n`
+    formatter: (color, variant, value) => {
+      return `  <draw:color draw:name="${color}-${variant}" draw:color="${value}" />\n`;
     },
-    'ext': 'soc',
-    'footer': '</ooo:color-table>'
+    ext: 'soc',
+    footer: '</ooo:color-table>'
   }
-}
+};
 
 for (const color in colors) {
   const element = colors[color];
@@ -87,7 +87,7 @@ for (const color in colors) {
   }
 }
 
-for (let key in formats) {
+for (const key in formats) {
   const format = formats[key];
   format.output.push(format.footer || '');
   fs.writeFile(`colors.${format.ext}`, format.output.join(''), 'utf8', (err) => {
